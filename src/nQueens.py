c8b6f36fd1c7005c7bc3affdf99e3f5ea34ae615
VakhshooriEhsan <52527299+VakhshooriEhsan@users.noreply.github.com>
2020-12-21 22:44:41 +0330

    Update nQueens.py

--- src/nQueens.py
+++ src/nQueens.py
@@ -1,37 +1,56 @@
 import bisect
 import math
-from numpy import random
-from numpy import zeros
+import numpy as np
 import matplotlib.pyplot as plt
 
-def Initialisation(n, m):
-    mems = []
-    for _ in range(n):
-        mems += [random.permutation(m).tolist()]
+def Initialisation(P, S, B):
+    mems = np.zeros((P, B), dtype=int)
+    for i in range(P):
+        for j in range(B-1):
+            mems[i][j] = np.random.randint(S-int(mems[i].sum())+1)
+        mems[i][B-1] = S - int(mems[i].sum())
     return mems
 
-def Fitness(mem):
-    penalty = 0
-    size = len(mem)
-    for i in range(size-1):
-        for j in range(i+1, size):
-            if mem[i]-i == mem[j]-j or mem[i]+i == mem[j]+j or mem[i] == mem[j]:
-                penalty += 1
-    return 1.0/(penalty+1)
+def Fitness(mems, mem):
+    score = 0.0
+    P = len(mems)
+    B = len(mem)
+    for i in range(P):
+        rival = mems[i]
+        tmp=0
+        for j in range(B):
+            if(mem[j] > rival[j]):
+                tmp += 1
+            elif(mem[j] < rival[j]):
+                tmp -= 1
+        if(tmp>0):
+            score += 1
+    return score/P
 
-def Parent_selection(mems, num):
-    size = len(mems)
+def Tournament_selection(mems, k):
+    P = len(mems)
     nmems = []
-    for i in random.permutation(size)[:num]:
+    for i in np.random.permutation(P)[:k]:
         nmems += [mems[i]]
-    nmems.sort(key=Fitness, reverse=True)
-    return [nmems[0], nmems[1]]
+    m0 = 0
+    m1 = 1
+    if(Fitness(mems, nmems[m0])<Fitness(mems, nmems[m1])):
+        m0, m1 = m1, m0
+    for i in range(2, k):
+        if(Fitness(mems, nmems[m1])<Fitness(mems, nmems[i])):
+            m1 = i
+        if(Fitness(mems, nmems[m0])<Fitness(mems, nmems[m1])):
+            m0, m1 = m1, m0
+    for i in range(k):
+        print(Fitness(mems, nmems[i]))
+        print(nmems[i])
+    return [nmems[m0], nmems[m1]]
 
 def Recombination(mem1, mem2, prob):
-    if(random.rand() > prob):
+    if(np.random.rand() > prob):
         return [mem1, mem2]
     size = len(mem1)
-    p = random.randint(1, size)
+    p = np.random.randint(1, size)
     nmem1 = mem1[0:p]
     nmem2 = mem2[0:p]
     for i in range(size):
@@ -42,10 +61,10 @@ def Recombination(mem1, mem2, prob):
     return [nmem1, nmem2]
 
 def Mutation(mem, prob):
-    if(random.rand() > prob):
+    if(np.random.rand() > prob):
         return mem
     size = len(mem)
-    p = random.permutation(size)[:2]
+    p = np.random.permutation(size)[:2]
     mem[p[0]], mem[p[1]] = mem[p[1]], mem[p[0]]
     return mem
 
@@ -65,7 +84,7 @@ def Plt_board(mems):
     n = len(mems[0])
     for i in range(size):
         plt.subplot(xb, yb, i+1)
-        chessboard = zeros((n, n))
+        chessboard = np.zeros((n, n))
         chessboard[1::2,0::2] = 1
         chessboard[0::2,1::2] = 1
         plt.imshow(chessboard, cmap='binary')
@@ -76,59 +95,65 @@ def Plt_board(mems):
 
 # Variables initialisation:
 
-n = 8 # number of queens | size of chessboard
-m = 100 # number of population
-num = 5 # number of select for parent selection
-Rprob = 1.0 # Recombination probability
-Mprob = 0.8 # Mutation probability
-T = 10000 # Termination condition
-data = [] # used for analyze
+S = 20 # number of soldiers
+B = 4 # number of battles
+P = 50 # number of population
+ELITISM = 0.1 # elitism probability
+
+# n = 8 # number of queens | size of chessboard
+# m = 100 # number of population
+K = 5 # number of select for Tournament selection
+# Rprob = 1.0 # Recombination probability
+# Mprob = 0.8 # Mutation probability
+# T = 10000 # Termination condition
+# data = [] # used for analyze
 
 
 # Genetic-algorithm:
 
-mems = Initialisation(m, n) # population initialisation
-mems.sort(key=Fitness, reverse=True) # sort members by fitness
-data += [mems.copy()]
+mems = Initialisation(P, S, B) # population initialisation
+# mems.sort(key=Fitness, reverse=True) # sort members by fitness
+# data += [mems.copy()]
 
-while Fitness(mems[0]) < 1 and T > 0 :
-    T -= 1
-    parents = Parent_selection(mems, num)
-    childs = Recombination(parents[0], parents[1], Rprob)
-    childs[0] = Mutation(childs[0], Mprob)
-    childs[1] = Mutation(childs[1], Mprob)
-    Survival_selection(mems, childs[0])
-    Survival_selection(mems, childs[1])
-    data += [mems.copy()]
+# while Fitness(mems[0]) < 1 and T > 0 :
+#     T -= 1
+parents = Tournament_selection(mems, K)
+print(parents)
+#     childs = Recombination(parents[0], parents[1], Rprob)
+#     childs[0] = Mutation(childs[0], Mprob)
+#     childs[1] = Mutation(childs[1], Mprob)
+#     Survival_selection(mems, childs[0])
+#     Survival_selection(mems, childs[1])
+#     data += [mems.copy()]
 
 
 # Analyze:
 
-maxFit = [] # max fitness
-k = 10
-avgFits = [] # avg of k-max fitness
-l = len(data) # last result
-t = 12 # number of result show
+# maxFit = [] # max fitness
+# k = 10
+# avgFits = [] # avg of k-max fitness
+# l = len(data) # last result
+# t = 12 # number of result show
 
-for i in range(l):
-    maxFit += [Fitness(data[i][0])]
-    tmp = 0
-    for j in range(min(k, len(data[i]))):
-        tmp += Fitness(data[i][j])
-    avgFits += [tmp/k]
+# for i in range(l):
+#     maxFit += [Fitness(data[i][0])]
+#     tmp = 0
+#     for j in range(min(k, len(data[i]))):
+#         tmp += Fitness(data[i][j])
+#     avgFits += [tmp/k]
 
-plt.figure(1)
-Plt_board(data[l-1][:t])
+# plt.figure(1)
+# Plt_board(data[l-1][:t])
 
-plt.figure(2)
-plt.subplot(2, 1, 1)
-plt.plot(maxFit, '.-')
-plt.title('Fitness Analyze')
-plt.ylabel('Max Fitness')
+# plt.figure(2)
+# plt.subplot(2, 1, 1)
+# plt.plot(maxFit, '.-')
+# plt.title('Fitness Analyze')
+# plt.ylabel('Max Fitness')
 
-plt.subplot(2, 1, 2)
-plt.plot(avgFits, '.-')
-plt.xlabel('Generations')
-plt.ylabel('Avg of k-max Fitness')
+# plt.subplot(2, 1, 2)
+# plt.plot(avgFits, '.-')
+# plt.xlabel('Generations')
+# plt.ylabel('Avg of k-max Fitness')
 
-plt.show()
+# plt.show()
